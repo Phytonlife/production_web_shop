ffd3b0e6467835dba14876e27fad65f9c980b7eb
Olzhas <110277959+Phytonlife@users.noreply.github.com>
2025-09-19 16:14:57 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,13 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Creating superuser if it does not exist..."
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin123!')
+"
+
 echo "Starting Gunicorn..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
-
