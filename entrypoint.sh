ac3806f77de69babd914762f72329e7280c5c3d2
Olzhas <110277959+Phytonlife@users.noreply.github.com>
2025-09-19 15:30:31 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-# Ожидаем, пока база данных станет доступной
-# Это важно, чтобы Django не пытался подключиться к еще не запущенной БД
-until nc -z db 5432; do
-  echo "Waiting for db to be ready..."
-  sleep 2
-done
+echo "Applying database migrations..."
+python manage.py migrate --noinput
 
-echo "Db is ready!"
-
-# Применяем миграции базы данных
-python manage.py migrate
-
-# Собираем статические файлы
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Запускаем Gunicorn
+echo "Starting Gunicorn..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
+
